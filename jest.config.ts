833fa120067b871d658caf7e37ebaa6f08e40264
Ryan-Fx <rinnd.dn@gmail.com>
2025-11-22 12:09:46 +0800

    chore: setup Jest with config and test scripts

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+// Gunakan path eksplisit "next/jest.js"
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  testEnvironment: "jest-environment-jsdom",
+  moduleDirectories: ["node_modules", "<rootDir>/src"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageProvider: "v8",
+};
+
+export default createJestConfig(customJestConfig);
