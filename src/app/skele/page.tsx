48439b5239b8d2e001104f19ef2d1fc13b8ad881
Ryan-Fx <rinnd.dn@gmail.com>
2025-11-22 18:28:30 +0800

    refactor: delete skeleton folder

--- src/app/skele/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import UserDetailSkeleton from "@/components/users/user-detail-skeleton";
-import UsersSkeleton from "@/components/users/users-skeleton";
-
-export default function Skele() {
-  return <UsersSkeleton />;
-}
